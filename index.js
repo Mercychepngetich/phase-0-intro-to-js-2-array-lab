accf52d522038c4d156876be9fe4744b8bdc08d5
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-07-13 16:13:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(cat) {
+    cats.push(cat);
+   
+    
+ }
+ function destructivelyPrependCat(cat) {
+    cats.unshift(cat);
+ }
+ function destructivelyRemoveLastCat(cat) {
+ cats.pop();
+}
+function destructivelyRemoveFirstCat(cat){
+    cats.shift();
+}
+function appendCat(cat){
+    return[...cats, cat];
+}
+function prependCat(cat){
+return[cat, ...cats];
+}
+function removeLastCat(){
+return cats.slice(0,-1);
+}
+function removeFirstCat(){
+return cats.slice(1);
+}
+
+
+
+
